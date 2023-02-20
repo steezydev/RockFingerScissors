2705240eeb112f8c21b5877f50f8a788b0c489a2
Ivan Ilichev <swggnuko.dev@gmail.com>
2023-02-20 15:43:42 +0300

    fix(dapp): :ambulance: removed legacy component import

--- dapp/src/pages/404.tsx
+++ dapp/src/pages/404.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { RiAlarmWarningFill } from 'react-icons/ri';
 
 import Layout from '@/components/layout/Layout';
-import ArrowLink from '@/components/links/ArrowLink';
 import Seo from '@/components/Seo';
 
 export default function NotFoundPage() {
@@ -18,9 +17,6 @@ export default function NotFoundPage() {
               className='drop-shadow-glow animate-flicker text-red-500'
             />
             <h1 className='mt-8 text-4xl md:text-6xl'>Page Not Found</h1>
-            <ArrowLink className='mt-4 md:text-lg' href='/'>
-              Back to Home
-            </ArrowLink>
           </div>
         </section>
       </main>
